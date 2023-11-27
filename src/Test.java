ccd3958ca126a8cb8ee772232c3ae520b42325aa
ayaka <ayaka.murakami1204@gmail.com>
2023-11-27 20:43:44 +0900

    instance ensyu4

--- src/Test.java
+++ src/Test.java
@@ -2,11 +2,15 @@
 public class Test {
 	public static void main(String[]args) {
 		Person taro=new Person();
-		taro.name="村上絢香";
-		taro.age=30;
+		taro.name="山田太郎";
+		taro.age=20;
+		taro.phoneNumber="090-1234-5678";
+		taro.address="東京";
 		
 		System.out.println(taro.name);
 		System.out.println(taro.age);
+		System.out.println(taro.phoneNumber);
+		System.out.println(taro.address);
 	}
 
 }
